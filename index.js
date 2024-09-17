7ab536df3a11195a8166ce4a328251dda4e4c1bd
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2024-09-17 19:01:12 +0600

    get and post and delete api done

--- index.js
+++ index.js
@@ -12,23 +12,75 @@ const users = [
     {id:4,name:'ATa',email:"ata@gamil.com"}
 ]
 
+// mosarofhossen5228
+// 7fKyW91h6NLyGpPm
+
+
+// mosarofhossen5228
+// GhmJoFbFwfxGpgG6
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://mosarofhossen5228:GhmJoFbFwfxGpgG6@cluster0.e4gyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+    const userCollection = client.db("usersDB").collection('users');
+
+
+    app.get('/users', async(req, res) => {
+      const cursor = userCollection.find();
+      const result =await cursor.toArray();
+      res.send(result);
+  })
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);//mongodb database a data insert hle sei datai hobe result;
+
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result);
+    })
+
+
+ 
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send("Users Management surver is running")
 })
-app.get('/users', (req, res) => {
-    res.send(users);
-})
-
-app.post('/users', (req, res) => {
-    console.log("responce is hitting");
-    console.log(req.body);
-    const newUser = req.body;
-    newUser.id = users.length + 1;
-    users.push(newUser);
-    res.send(newUser);
-})
-
-
 
 app.listen(port, () => {
     console.log(`users-Management-System Server is Running on port ${port}`)
